65b931902dd722a32786fe430db9a6ae27980478
Jereen <jader9e1@hotmail.com>
2023-06-09 08:23:56 -0500

    "feat: Moved src and public files out of client and deleted client folder"

---
+++ src/Contact.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ContactInput from "./ContactInput";
+import Axios from "axios";
+
+
+function Contact(props){
+
+    const displayInfo2= (contactInfo)=>{
+    //   console.log(contactInfo);
+      Axios.post('${process.env.CREATE_PATH}', {contactInfo})
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
+    }
+
+
+    return (
+            <div className="sections contact-box">
+                <div className="rows contact-style">
+                    
+                    <div className="contact-col">
+                    <div>Want to hire or say hello?</div>
+                    <img src="images/contact/Hands Typing.svg" alt="contact background" ></img>
+                    </div>
+                    <div className="contact-col">
+                        <ContactInput displayInfo={displayInfo2}/>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+    )
+}
+
+
+
+export default Contact;
